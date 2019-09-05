4e6d932a57117bc700b2628f2bd8074543fc53cb
utkarsh21 <utk.set@gmail.com>
2019-09-05 10:01:12 +0000

    First commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      Dummy Demo Page
+      <Link to="/">Go back to home page!</Link>
+    </div>
+  );
+};
